b02ff824b2d191f3339e1ba5de1f4dd291ebe987
mortenwh <morten.wergeland.hansen@gmail.com>
2017-03-16 14:55:15 +0100

    small bug fix..

--- svp_drifters/models.py
+++ svp_drifters/models.py
@@ -3,9 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 from geospaas.catalog.models import Dataset as CatalogDataset
-from geospaas.svp_drifters.managers import SVPDrifterManager
+from svp_drifters.managers import SVPDrifterManager
 
-class LanceBuoy(CatalogDataset):
+class SVPDrifter(CatalogDataset):
     class Meta:
         proxy = True
     objects = SVPDrifterManager()
