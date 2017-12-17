bbf5171e959bacf16084f4a3b18fd1fd01810d9b
Artem Moiseev <korvinos@bk.ru>
2017-12-17 15:02:07 +0100

    Updated ingest command for django 1.11.7

--- svp_drifters/management/commands/ingest_svp_drifters.py
+++ svp_drifters/management/commands/ingest_svp_drifters.py
@@ -1,10 +1,9 @@
 from dateutil.parser import parse
-
-from django.utils import timezone
 from django.core.management.base import BaseCommand
 
 from geospaas.utils import uris_from_args
 from svp_drifters.models import SVPDrifter
+
 
 class Command(BaseCommand):
     args = '''<metadata filename> <data filename>
@@ -18,6 +17,12 @@ class Command(BaseCommand):
     help = 'Add drifter metadata to archive'
 
     def add_arguments(self, parser):
+        parser.add_argument('data_file',
+                            nargs=1,
+                            type=str)
+        parser.add_argument('metadata_file',
+                            nargs=1,
+                            type=str)
         parser.add_argument('--start',
                             action='store',
                             default='2014-01-01',
@@ -48,8 +53,10 @@ class Command(BaseCommand):
                             help='''Maximum number of drifters''')
 
     def handle(self, *args, **options):
-        if not len(args)>=2:
+        print(options)
+        if not options['data_file'] or not options['metadata_file']:
             raise IOError('Please provide two filenames')
+
         start = parse(options['start'])
         stop = parse(options['stop'])
         minlon = float(options['minlon'])
@@ -57,15 +64,12 @@ class Command(BaseCommand):
         minlat = float(options['minlat'])
         maxlat = float(options['maxlat'])
         maxnum = eval(options['maxnum'])
-        
-        uris = uris_from_args(args[0], args[1])
-        count = SVPDrifter.objects.get_or_create(uris[0], uris[1],
-                time_coverage_start=start,
-                time_coverage_end=stop,
-                minlat=minlat,
-                maxlat=maxlat,
-                minlon=minlon,
-                maxlon=maxlon,
-                maxnum=maxnum)
-        print 'Added %d new drifter datasets'%count
+        data_uri, metadata_uri = map(uris_from_args, [options['data_file'], options['metadata_file']])
+        count = SVPDrifter.objects.get_or_create(metadata_uri[0], data_uri[0],
+                                                 time_coverage_start=start,
+                                                 time_coverage_end=stop,
+                                                 minlat=minlat, maxlat=maxlat,
+                                                 minlon=minlon, maxlon=maxlon,
+                                                 maxnum=maxnum)
+        print 'Added %d new drifter datasets' % count
 
