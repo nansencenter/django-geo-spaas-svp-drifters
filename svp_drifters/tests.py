a3ccf92ba59a5d9f55704e998cf9486221afb5dc
Artem Moiseev <korvinos@bk.ru>
2017-12-19 10:12:15 +0100

    Set metadata test

--- svp_drifters/tests.py
+++ svp_drifters/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from svp_drifters.models import SVPDrifter
+from geospaas.vocabularies.models import Platform, Instrument, DataCenter, ISOTopicCategory
+from geospaas.catalog.models import GeographicLocation, DatasetURI, Source, Dataset
 
-# Create your tests here.
+
+class SVPDrifterModelTest(TestCase):
+    fixtures = ['vocabularies']
+
+    def test_set_metadata(self):
+        src, dc, iso = SVPDrifter.objects.set_metadata()
+
+        self.assertIsInstance(src, Source)
+        self.assertEqual(src.platform.short_name, 'BUOYS')
+        self.assertEqual(src.instrument.short_name, 'DRIFTING BUOYS')
+
+        self.assertIsInstance(dc, DataCenter)
+        self.assertEqual(dc.short_name, 'DOC/NOAA/OAR/AOML')
+
+        self.assertIsInstance(iso, ISOTopicCategory)
+        self.assertEqual(iso.name, 'Oceans')
